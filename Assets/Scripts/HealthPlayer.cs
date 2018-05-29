c62af1b3c751ffe0b23442eb8e1de843475c82e4
Savagerqt <tanguy.viv@gmail.com>
2018-05-29 10:49:59 +0200

    Barre de Sueur 2

--- Assets/Scripts/HealthPlayer.cs
+++ Assets/Scripts/HealthPlayer.cs
@@ -7,14 +7,18 @@ using UnityEngine.UI;
 public class HealthPlayer : MonoBehaviour {
 
 	private int _health; 			// Barre de vie du joueur
-	public int _hpmax;
 	private float _sweat; 			// Barre de sueur du joueur
-	public int _swtmax;
 	private Image _healthBar;		// Image de la barre de vie
 	private Image _sweatBar; 		// Image de la barre de sueur
 	private Rigidbody _rb; 			// Rigid Body du cycliste
+	private CyclistMovement _cm;	// Relatif au script de mouvement du cycliste
 
-	public float SWT_COEF;			// Paramètre pour régler la vitesse de remplissage de la sueur
+	// Variables publiques
+	public float SWT_GENERATION_COEF;			// Paramètre pour régler la vitesse de remplissage de la sueur
+	public float SWT_LOSS_COEF; 				// Paramètre pour régler la vitesse de perte de sueur
+	public int _swtmax;
+	public int _hpmax;
+
 
 
 
@@ -27,6 +31,7 @@ public class HealthPlayer : MonoBehaviour {
 		_rb = GameObject.Find("CyclistDos").GetComponent<Rigidbody>();
 		_healthBar = GameObject.Find("HealthBar").GetComponent<Image>();
 		_sweatBar = GameObject.Find ("SweatBar").GetComponent<Image> ();
+		_cm = GameObject.Find ("CyclistDos").GetComponent<CyclistMovement> ();
 
 		// On commence avec une barre de sueur nulle
 		_sweatBar.fillAmount = 0;
@@ -36,7 +41,7 @@ public class HealthPlayer : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		sweat ();
-		updateSweat (); 
+		UpdateSweat (); 
 
 	}
 
@@ -75,7 +80,7 @@ public class HealthPlayer : MonoBehaviour {
 		_healthBar.fillAmount = amount;
 	}
 
-	public void updateSweat() {
+	public void UpdateSweat() {
 		// Met à jour la barre de sueur 
 		float amount = (float) _sweat / _swtmax; 
 		_sweatBar.fillAmount = amount; 
@@ -119,12 +124,49 @@ public class HealthPlayer : MonoBehaviour {
 	//===============================================================================================
 
 	public void sweat() {
-		if (_sweat < _swtmax) {
-			// Fonction pour remplir la barre de sueur
-			// La barre de sueur se remplit proportionnellement à la vitesse du personnage
-			Vector3 speed = _rb.velocity;
-			_sweat += SWT_COEF * speed.sqrMagnitude;
+		/*	La fonction sweat gère la génération de sueur */ 
+
+		// IMPORTANT 
+		// PLutot que de générer la sueur proportionnellement à la vitesse, il serait 
+		// plus intéressant de le faire par rapport à la force (ou resistance) exercée 
+		// par le pédalier !
+
+		if (_cm.getForward() > 0) {
+			// Tant que le cycliste pédale, on ajoute de la sueur
+
+			if (_sweat < _swtmax) {
+				// La barre de sueur se remplit proportionnellement à la vitesse du personnage
+				Vector3 speed = _rb.velocity;
+				_sweat += SWT_GENERATION_COEF * Mathf.Sqrt(speed.sqrMagnitude);
+			}
+		}
+		else {
+			// Sinon on ne pédale plus, et la sueur doit se mettre à baisser 
+			if (_sweat > 0) {
+				// La barre de sueur diminue toujours de manière constante
+				// (On peut éventuellement mettre un rapport entre vitesse du vélo 
+				// et perte de sueur)
+				_sweat -= SWT_LOSS_COEF;
+			}	
+			if (_sweat < 0) {
+				// Si la sueur devient négative, on la rétablit à zéro
+				_sweat = 0;
+			}
 		}
 	}
+
+
+	public void refresh(int amount) {
+		/* La fonction refresh permet de diminuer la sueur du personnage */ 
+		_sweat -= amount;
+
+		if (_sweat <0) {
+			_sweat = 0;
+		}
+		// On appelle la fonction pour mettre à jour la barre de vie
+		Debug.Log("Refreshing !");
+		UpdateSweat ();
+	}
+
 
 }
